ab63e6da93294a96014de5a33763df07b127fb59
Gauthier Delamarre <gdelamarre@gmail.com>
2019-12-11 11:58:56 +0100

    Add selupdate command

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "Publishing deckle.phar on deckle.adimeo.eu"
 scp deckle.phar root@deckle.adimeo.eu:/var/www/html/deckle/public/releases/latest.phar
-"$(deckle.phar version)" > .version
 scp .version root@deckle.adimeo.eu:/var/www/html/deckle/public/releases/latest.version
 echo "Done!"

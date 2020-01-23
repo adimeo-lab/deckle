d6b375ec87d39aebcc1d67db3c16c0501e39d209
Boris CERATI <contact@boris-cerati.fr>
2020-01-23 10:01:28 +0100

    Build the images when resetting the project on the VM

--- src/Command/Deckle/Up.php
+++ src/Command/Deckle/Up.php
@@ -5,7 +5,6 @@ namespace Adimeo\Deckle\Command\Deckle;
 
 use Adimeo\Deckle\Command\AbstractDeckleCommand;
 use Adimeo\Deckle\Deckle;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -31,12 +30,18 @@ class Up extends AbstractDeckleCommand
         $reset = $input->getOption('reset');
 
         Deckle::print('Starting project <info>%s</info>', $conf['project']['name']);
-
+        
         Deckle::runCommand('init', ['--no-interaction' => true, '--reset' => $reset]);
         Deckle::events()->trigger('up.init.post');
         Deckle::runCommand('push', ['--no-interaction' => true, '--reset' => $reset]);
         Deckle::events()->trigger('up.push.post');
-        Deckle::runCommand('docker:compose', ['--no-interaction' => true, 'args' => ['up', '-d']]);
+
+        $dcArgs = ['up', '-d'];
+        if ($reset) {
+            $dcArgs[] = '--build';
+        }
+
+        Deckle::runCommand('docker:compose', ['--no-interaction' => true, 'args' => $dcArgs]);
         Deckle::events()->trigger('up.dc-up.post');
         Deckle::runCommand('mutagen:sync', ['--no-interaction' => true, 'cmd' => 'start', '--force' => true]);
         Deckle::runCommand('mutagen:monitor', ['--no-interaction' => true, '--until-sync' => true]);
